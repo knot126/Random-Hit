8bcde5bb8638c70ef51912f14e91a24b1172c06e
cdde <3018231-cdde@users.noreply.gitlab.com>
2020-05-05 14:27:15 -0500

    Update to WinBuild.bat

--- WinBuild.bat
+++ WinBuild.bat
@@ -1,12 +1,13 @@
+@echo off
 echo Delete files from %1...
-del /s /q %1\assets
-del /s /q %1\res
-del /s /q %1\AndroidManifest.xml
+del /F /S /Q %1\assets
+del /F /S /Q %1\res
+del /F /S /Q %1\AndroidManifest.xml
 
 echo Copy files to %1...
-xcopy .\assets %1
-xcopy .\res %1
-xcopy .\AndroidManifest.xml %1
+xcopy /J /Y /E .\assets %1
+xcopy /J /Y /E .\res %1
+xcopy /J /Y /E .\AndroidManifest.xml %1
 
 echo 
 echo If you would like to prepare for a game modes build, then edit the files
